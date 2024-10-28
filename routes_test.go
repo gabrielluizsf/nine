74f09cd09f5edd7326542067fb9021d3c1233879
gabrielluizsf <93672097+gabrielluizsf@users.noreply.github.com>
2024-10-28 09:26:03 -0300

    test: adding unit tests for route handling and middleware functionality
    - Created a test case in `TestRoutes` to validate the correct sorting of routes based on patterns.
    - Added assertions to confirm that the expected route patterns are equivalent to the actual sorted routes.
    - Implemented tests for the HTTP GET method to ensure it correctly responds with a 200 status code and the expected JSON payload when accessing the `/user` endpoint.
    - Included a verification for the 404 status code when accessing an unspecified route (`/`), ensuring the `notFoundMiddleware` functions correctly.

---
+++ routes_test.go
@@ -0,0 +1,45 @@
+package nine
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"sort"
+	"testing"
+
+	"github.com/i9si-sistemas/assert"
+)
+
+func TestRoutes(t *testing.T) {
+	r := make(Routes, 0)
+	if r.Len() != len(r) {
+		t.FailNow()
+	}
+	r = append(r, Router{pattern: "/"})
+	r = append(r, Router{pattern: "/user/{id}"})
+	r = append(r, Router{pattern: "/user"})
+	sort.Sort(r)
+	expected := Routes{{pattern: "/"}, {pattern: "/user"}, {pattern: "/user/{id}"}}
+	assert.Equal(t, r, expected, "should be equal")
+
+	server := NewServer(42)
+
+	payload := JSON{
+		"user": JSON{
+			"id":   1,
+			"name": "gabrielluizsf",
+		},
+	}
+	server.Get("/user", func(req *Request, res *Response) error {
+		return res.JSON(payload)
+	})
+	req := httptest.NewRequest(http.MethodGet, "/user", nil)
+	w := server.Test().Request(req)
+	assert.Equal(t, w.Code, 200, "status code should be 200")
+	responseBytes := w.Body.Bytes()
+	b, err := payload.Bytes()
+	assert.NoError(t, err, "expected no error")
+	assert.Equal(t, responseBytes[:len(responseBytes)-1], b, "should be equal bytes")
+	req = httptest.NewRequest(http.MethodGet, "/", nil)
+	w = server.Test().Request(req)
+	assert.Equal(t, w.Code, 404, "status should be 404")
+}
