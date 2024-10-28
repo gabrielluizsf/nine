16491938192bcca09e5676a27e9441445ac8223a
gabrielluizsf <93672097+gabrielluizsf@users.noreply.github.com>
2024-10-28 09:02:46 -0300

    - feat: adding a `notFoundMiddleware` to handle 404 errors when a route is not found. - Enhanced the `patternExists` method to utilize binary search for improved efficiency in route pattern matching. - Updated the `registerRoutes` method to include the new not found middleware for all route handlers. - Introduced sorting for routes using the `Routes` type to ensure proper order for binary search.

---
+++ routes.go
@@ -0,0 +1,15 @@
+package nine
+
+type Routes []Router
+
+func (r Routes) Len() int {
+	return len(r)
+}
+
+func (r Routes) Less(i, j int) bool {
+	return r[i].pattern < r[j].pattern
+}
+
+func (r Routes) Swap(i, j int) {
+	r[i], r[j] = r[j], r[i]
+}
