6bd7e3c4eea9cde7f62ce6c65158558f59991eda
gabrielluizsf <93672097+gabrielluizsf@users.noreply.github.com>
2025-04-16 12:29:26 -0300

    feat: add Server interface and update NewServer function
    This commit introduces a `Server` interface to provide a high-level abstraction for creating and managing HTTP servers.
    The `NewServer` function is updated to return a `Server` interface instead of a concrete `server.Server` instance.
    This change enhances the flexibility and testability of the nine package.

--- server.go
+++ server.go
@@ -1,7 +1,29 @@
 package nine
 
-import "github.com/i9si-sistemas/nine/pkg/server"
+import (
+	"context"
 
-func NewServer[T string | int](port T) *server.Server {
+	"github.com/i9si-sistemas/nine/pkg/server"
+)
+
+// Server is a high-level abstraction for creating and managing HTTP servers using nine.
+type Server interface {
+	Use(middlewares any) error
+	Get(string, ...any) error
+	Post(string, ...any) error
+	Put(string, ...any) error
+	Patch(string, ...any) error
+	Delete(string, ...any) error
+	Route(string, func(*server.RouteGroup))
+	Group(string, ...any) *server.RouteGroup
+	ServeFiles(string, string)
+	Test() *server.TestServer
+	Listen() error
+	Shutdown(ctx context.Context) error
+}
+
+// NewServer returns a new (server.Server) instance bound to the specified port.
+// It accepts both integer and string types for the port.
+func NewServer[T string | int](port T) Server {
 	return server.New(port)
 }
