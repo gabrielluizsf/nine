dea5242dc734bddc840792b548115f60e903410c
gabrielluizsf <93672097+gabrielluizsf@users.noreply.github.com>
2025-05-16 11:05:15 -0300

    feat: add cache control headers and change redirect status code
    This commit modifies the `Redirect` handler to include cache control headers to prevent caching of redirects. It also changes the redirect status code from `http.StatusMovedPermanently` (301) to `http.StatusFound` (302) to ensure that clients always follow the redirect, even after the first request.

--- pkg/server/handler.go
+++ pkg/server/handler.go
@@ -15,7 +15,11 @@ func (h HandlerWithContext) Handler() Handler {
 
 func (h Handler) Redirect(url string) Handler {
 	return func(req *Request, res *Response) error {
-		http.Redirect(res.HTTP(), req.HTTP(), url, http.StatusMovedPermanently)
+		w := res.HTTP()
+		w.Header().Set("Cache-Control", "no-store, no-cache, must-revalidate")
+		w.Header().Set("Pragma", "no-cache")
+		w.Header().Set("Expires", "0")
+		http.Redirect(w, req.HTTP(), url, http.StatusFound)
 		return nil
 	}
 }
