eda7f9eed5a9097960ba6854e7b3f5457915cd57
gabrielluizsf <93672097+gabrielluizsf@users.noreply.github.com>
2025-07-17 10:12:47 -0300

    refactor: enhance error handling in executeRequest and introduce RequestError type

--- internal/client/request.go
+++ internal/client/request.go
@@ -1,14 +1,61 @@
 package client
 
 import (
+	"bytes"
 	"context"
 	"io"
 	"net/http"
+
+	"github.com/i9si-sistemas/nine/internal/json"
+	"github.com/i9si-sistemas/nine/internal/xml"
 	public "github.com/i9si-sistemas/nine/pkg/client"
 )
 
 func newRequest(ctx context.Context, method, url string, body io.Reader) (*http.Request, error) {
 	return http.NewRequestWithContext(ctx, method, url, body)
+}
+
+type RequestError struct {
+	StatusCode int
+	Payload    io.Reader
+}
+
+func NewRequestError(err error) *RequestError {
+	return &RequestError{
+		Payload: bytes.NewBuffer([]byte(err.Error())),
+	}
+}
+
+func (err *RequestError) Error() string {
+	r := new(bytes.Buffer)
+	_, _ = io.Copy(r, err.Payload)
+	b := make([]byte, 0, 512)
+	for {
+		n, err := r.Read(b[len(b):cap(b)])
+		b = b[:len(b)+n]
+		if err != nil {
+			if err == io.EOF {
+				err = nil
+			}
+			return string(b)
+		}
+
+		if len(b) == cap(b) {
+			b = append(b, 0)[:len(b)]
+		}
+	}
+}
+
+type JSON map[string]any
+
+func (err *RequestError) JSON() (payload JSON) {
+	_ = json.NewDecoder(err.Payload).Decode(&payload)
+	return
+}
+
+func (err *RequestError) XML() (payload JSON) {
+	payload, _ = xml.Decode(err.Payload)
+	return payload
 }
 
 // executeRequest prepares and sends an HTTP request with the specified method, URL, and options.
@@ -23,7 +70,7 @@ func newRequest(ctx context.Context, method, url string, body io.Reader) (*http.
 // Returns:
 //   - *http.Response: the HTTP response received from the server.
 //   - error: an error if the request preparation or execution failed, or nil if it was successful.
-func (c *client) executeRequest(method, url string, options *public.Options) (*http.Response, error) {
+func (c *client) executeRequest(method, url string, options *public.Options) (*http.Response, *RequestError) {
 	var (
 		headers     = options.Headers
 		body        = options.Body
@@ -33,8 +80,18 @@ func (c *client) executeRequest(method, url string, options *public.Options) (*h
 
 	req, err := newRequest(c.ctx, method, url, body)
 	if err != nil {
-		return nil, err
+		return nil, NewRequestError(err)
 	}
 	public.SetHeaders(req, headers)
-	return c.Response(req)
+	res, err := c.Response(req)
+	if err != nil {
+		return nil, NewRequestError(err)
+	}
+	if res.StatusCode >= http.StatusBadRequest {
+		return res, &RequestError{
+			StatusCode: res.StatusCode,
+			Payload:    res.Body,
+		}
+	}
+	return res, nil
 }
